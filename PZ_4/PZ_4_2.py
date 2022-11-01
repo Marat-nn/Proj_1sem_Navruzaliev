65d811676219d9ad3be859037d7a322013ad2965
ergudz <ergudz@gmail.com>
2022-11-01 16:28:06 +0300

    Написал условие к задачам

--- PZ_4/PZ_4_2.py
+++ PZ_4/PZ_4_2.py
@@ -1,15 +1,19 @@
+# Дано целое число N(>0). С помощью операций деления нацело и взятия остатка от деления определить,
+# имеется ли в записи числа N цифра "2".
+# Если имеется, то вывести TRUE, если нет - вывести FALSE.
+
 n = int(input("Введите число: "))
 a = n
 i = 0
 
-rezult = False
+result = False
 
 while a >= 1:
     i += 1
     r = a % 10
     if r == 2:
-        rezult = True
+        result = True
         break
     a = int(a / 10)
 
-print(rezult)
+print(result)
