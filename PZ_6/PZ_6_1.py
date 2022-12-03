3a016331330eb1b83a5c410c080137336f1151b2
Marat <marat.nav@mail.ru>
2022-12-03 17:15:44 +0300

    Сделал ПЗ 6 задание 1

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,5 +1,33 @@
 # Дан целочисленный список размера N. Увеличить все четные числа, содержащиеся в списке,
 # на исходное значение первого четного числа. Если четные числа в списке отсутствуют,
 # то оставить список без изменений
+from random import random
 
-def()
+size = int(input("Введите размер списка "))
+N = []
+
+for i in range(size):
+    N.append(int(random() * 100))
+
+print(N)
+
+
+def tofindfirsteven():
+    first_even = 0
+    n = 0
+    c = True
+    while c:
+        if N[n] % 2 == 0:
+            first_even = N[n]
+            c = False
+        n += 1
+    return first_even
+
+
+even_number = tofindfirsteven()
+print(even_number)
+for i in range(len(N)):
+    if N[i] % 2 == 0:
+        N[i] += even_number
+
+print(N)
